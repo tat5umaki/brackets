9ed2830380b862fcfb2371adbda50758a8bb369e
Baranau Ruslan <baranauruslan@gmail.com>
2024-08-21 16:24:43 +0300

    feat: add func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 > 0) return false;
+    
+    const openBrackets = [];
+    const stack = [];
+    const bracketsPairs = {};
+
+    bracketsConfig.forEach(bracket => {
+        openBrackets .push(bracket[0]);
+        bracketsPairs[bracket[1]] = bracket[0];
+    })
+
+    for (let i = 0; i < str.length; i++) {
+        let currentSymbol = str[i];
+
+        if (openBrackets .includes(currentSymbol)) {
+            if (bracketsPairs[currentSymbol] == stack[stack.length - 1] && stack.length != 0)
+                stack.pop();
+            else
+                stack.push(currentSymbol);
+        } else {
+            if (stack.length == 0) 
+                return false;
+
+            if (bracketsPairs[currentSymbol] == stack[stack.length - 1])
+                stack.pop();
+            else
+                return false;
+        }
+    }
+
+    return stack.length == 0;
 }
